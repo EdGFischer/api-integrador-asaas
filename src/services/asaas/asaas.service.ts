7c5162834ec39af87ad7618710ffbfb436a0eb49
EdGFischer <eduardogfischer@gmail.com>
2025-05-14 10:33:33 -0300

    feat: added delete and update to asaas service

--- src/services/asaas/asaas.service.ts
+++ src/services/asaas/asaas.service.ts
@@ -6,7 +6,7 @@ import { Env } from 'src/env';
 
 @Injectable()
 export class AsaasService {
-  private readonly baseUrl = 'https://www.asaas.com/api/v3/customers';
+  private readonly baseUrl = 'https://api-sandbox.asaas.com/v3/customers';
   private readonly apiKey: string;
 
   constructor(
@@ -23,9 +23,42 @@ export class AsaasService {
     phone: string;
     address: string;
   }): Promise<{ id: string }> {
+    
     const response = await firstValueFrom(
       this.httpService.post(
         this.baseUrl,
+        {
+          name: data.name,
+          email: data.email,
+          cnpj: data.cpfCnpj,
+          phone: data.phone,
+          address: data.address,
+        },
+        {
+          headers: {
+            accept: 'application/json', 
+            'content-type': 'application/json',
+            access_token: `${this.apiKey}`,
+          },
+        },
+      ),
+    );
+
+    return response.data;
+  }
+
+
+  async updateCustomer(id: string, data: {
+    name?: string;
+    email?: string;
+    cpfCnpj?: string;
+    phone?: string;
+    address?: string;
+  }): Promise<{ id: string }> {
+
+    const response = await firstValueFrom(
+      this.httpService.put(
+        `${this.baseUrl}/${id}`,
         {
           name: data.name,
           email: data.email,
@@ -35,7 +68,9 @@ export class AsaasService {
         },
         {
           headers: {
-            Authorization: `Bearer ${this.apiKey}`,
+            accept: 'application/json',
+            'content-type': 'application/json',
+            access_token: `${this.apiKey}`,
           },
         },
       ),
@@ -43,4 +78,16 @@ export class AsaasService {
 
     return response.data;
   }
+
+async deleteCustomer(asaasCustomerId: string): Promise<void> {
+  await firstValueFrom(
+    this.httpService.delete(`${this.baseUrl}/${asaasCustomerId}`, {
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        access_token: this.apiKey,
+      },
+    }),
+  );
+}
 }
